2d3145501edb781ca29a20e18602bf258cf8cb6a
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-67.local>
2018-03-23 09:03:46 -0400

    Classes Setup

---
+++ Client/src/main/java/ID.java
@@ -0,0 +1,41 @@
+public class ID {
+
+    public ID(String uderID, String name, String gitHub) {
+        this.userID = userID;
+        this.name = name;
+        this.github = github;
+    }
+    public ID(String nme, String github) {
+        this.name = name;
+        this.github = github;
+    }
+
+    private String userID;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    private String name;
+    private String github;
+}
---
+++ Client/src/main/java/Messages.java
@@ -0,0 +1,63 @@
+public class Messages {
+    private String sequence;
+    private String timestamp;
+    private String fromID;
+    private String toID;
+    private String message;
+
+    public Messages(String sequence, String timestamp, String fromID, String toID, String message) {
+        this.sequence = sequence;
+        this.timestamp = timestamp;
+        this.fromID = fromID;
+        this.toID = toID;
+
+        this.message = message;
+    }
+
+    public Messages(String fromID, String toID, String message) {
+        this.fromID = fromID;
+        this.toID = toID;
+        this.message = message;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getFromID() {
+        return fromID;
+    }
+
+    public void setFromID(String fromID) {
+        this.fromID = fromID;
+    }
+
+    public String getToID() {
+        return toID;
+    }
+
+    public void setToID(String toID) {
+        this.toID = toID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
